ad594fbe930f6ff5bf9946c03a0422c70a575baf
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2020-04-19 19:30:11 -0400

    Tying to display drawer

---
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -0,0 +1,27 @@
+package com.codepath.android.navigationdrawerexercise.activities;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.codepath.android.navigationdrawerexercise.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+    }
+}
