cffddc538b87f8ef38efa7c6ab61e53aa357abf4
Cleevens CHARLEMAGNE <48931437+CleevensCharlemagne@users.noreply.github.com>
2023-01-14 20:56:53 -0500

    update

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.codepath.android.navigationdrawerexercise.R;
 
-//N
+//Na
 public class MainActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
