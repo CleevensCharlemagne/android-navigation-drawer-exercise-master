36ae23566297899312fdfc297afab429b072ff98
Cleevens CHARLEMAGNE <48931437+CleevensCharlemagne@users.noreply.github.com>
2023-01-25 21:04:13 -0500

    update

--- app/src/main/java/com/codepath/android/navigationdrawerexercise/fragments/FuturamaFragment.java
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/fragments/FuturamaFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 
 import com.codepath.android.navigationdrawerexercise.R;
 
+// Creating the family guy fragment
 public class FuturamaFragment extends Fragment {
 
     @Override
